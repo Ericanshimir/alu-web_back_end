25f209ebf37eda42727310ccf55a23021717458e
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2024-06-26 20:34:11 +0300

    Update 5-app.py

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Basic Flask app that implements i18n and internationalization"""
+"""Basic Flask app that implements i18n and internationalizatin"""
 
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
