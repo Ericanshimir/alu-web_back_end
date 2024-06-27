a6bc13acab14d93eccd02d0b6a090339936882c9
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2024-06-27 20:10:48 +0300

    Update 3-app.py

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -29,7 +29,7 @@ def get_locale():
 @app.route('/')
 def index():
     """hello world"""
-    return render_template("3-index.html", message="Welcome to Holberton")
+    return render_template("3-index.html", message="Welcomelberton")
 
 
 if __name__ == "__main__":
