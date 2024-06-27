cea667d09ec70d31cb7096377535d97d564b2228
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2024-06-27 19:49:36 +0300

    4-app.py

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -2,7 +2,7 @@
 """Basic Flask app that implements i18n and internationalization"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, get_locale, get_timezone
 
 app = Flask(__name__)
 
@@ -15,22 +15,23 @@ class Config:
 
 
 app.config.from_object(Config)
-babel = Babel(app)
+babel = Babel(app, locale_selector=get_locale)
 
 
-@babel.localeselector
+
 def get_locale():
     """Get locale for your application"""
     locale = request.args.get('locale')
     if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     """Home page for your application"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
