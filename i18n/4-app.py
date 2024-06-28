b4339b5413596474cb33c441fa09fd090f6bc028
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2024-06-29 00:31:32 +0300

    Update 4-app.py

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -25,7 +25,6 @@ def get_locale():
     if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
