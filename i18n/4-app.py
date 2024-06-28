65b61f43a7c7daef736a3c32fa6b526636a8d0b4
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2024-06-29 00:32:38 +0300

    4-app.py

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -18,7 +18,6 @@ app.config.from_object(Config)
 babel = Babel(app, locale_selector=get_locale)
 
 
-
 def get_locale():
     """Get locale for your application"""
     locale = request.args.get('locale')
