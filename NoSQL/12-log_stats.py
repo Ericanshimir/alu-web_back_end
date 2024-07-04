5cdafecc19583e1ba6001b99cb433e7e3f522ef6
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2024-07-04 15:49:06 +0300

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,28 +2,28 @@
 """python scripts"""
 from pymongo import MongoClient
 
-
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-
 def log_stats(mongo_collection, option=None):
-    """ script that provides some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
+    """ script that provides some stats about Nginx logs stored in MongoDB """
     if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
+        value = mongo_collection.count_documents({"method": option})
         print(f"\tmethod {option}: {value}")
         return
 
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
+    # Total count of logs
+    total_logs = mongo_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Count for each HTTP method
     print("Methods:")
     for method in METHODS:
-        log_stats(nginx_collection, method)
+        method_count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    # Status check count
     status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
-
 
 if __name__ == "__main__":
     nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
